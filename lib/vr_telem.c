d46df913ad3778f305df5d38ee4f089b4e43a5d1
Andrew Pullin <pullin@berkeley.edu>
2014-12-18 19:39:13 -0800

    Updating vr_telem to remove externs

--- lib/vr_telem.c
+++ lib/vr_telem.c
@@ -3,6 +3,9 @@
 // XC compiler include
 #include <xc.h>
 
+// Project settings
+#include "settings.h"
+
 // imageproc-lib includes
 #include "utils.h"
 #include "vr_telem.h"
@@ -18,14 +21,11 @@
 //extern int bemf[NUM_MOTOR_PIDS];
 
 //externs added back in for VR telem porting (pullin 10/9/14)
-extern int bemf[NUM_PIDS];
-extern pidPos pidObjs[NUM_PIDS];
+//extern int bemf[NUM_PIDS];
+//extern pidPos pidObjs[NUM_PIDS];
 
 //void vrTelemGetData(unsigned char* ptr) {
 void vrTelemGetData(vrTelemStruct_t* ptr) {
-    
-    //vrTelemStruct_t* tptr;
-    //tptr = (vrTelemStruct_t*) ptr;
 
     int gdata[3];   //gyrodata
     int xldata[3];  // accelerometer data
@@ -33,17 +33,14 @@ void vrTelemGetData(vrTelemStruct_t* ptr) {
     mpuGetGyro(gdata);
     mpuGetXl(xldata);
 
-    ptr->posL = pidObjs[0].p_state;
-    ptr->posR = pidObjs[1].p_state;
-    ptr->composL = pidObjs[0].p_input + pidObjs[0].interpolate;
-    ptr->composR = pidObjs[1].p_input + pidObjs[1].interpolate;
-    ptr->dcL = pidObjs[0].output; // left
-    ptr->dcR = pidObjs[1].output; // right
-    ptr->bemfL = bemf[0];
-    ptr->bemfR = bemf[1];
-
-    mpuGetGyro(gdata);
-    mpuGetXl(xldata);
+    ptr->posL = pidGetPState(LEFT_LEGS_PID_NUM);
+    ptr->posR = pidGetPState(RIGHT_LEGS_PID_NUM);
+    ptr->composL = pidGetPInput(LEFT_LEGS_PID_NUM) + pidGetInterpolate(LEFT_LEGS_PID_NUM);
+    ptr->composR = pidGetPInput(RIGHT_LEGS_PID_NUM) + pidGetInterpolate(RIGHT_LEGS_PID_NUM);
+    ptr->dcL = pidGetOutput(LEFT_LEGS_PID_NUM); // left
+    ptr->dcR = pidGetOutput(RIGHT_LEGS_PID_NUM); // right
+    ptr->bemfL = pidGetBEMF(LEFT_LEGS_PID_NUM);
+    ptr->bemfR = pidGetBEMF(RIGHT_LEGS_PID_NUM);
 
     ptr->gyroX = gdata[0];
     ptr->gyroY = gdata[1];
@@ -56,6 +53,6 @@ void vrTelemGetData(vrTelemStruct_t* ptr) {
 
 //This may be unneccesary, since the telemtry type isn't totally anonymous
 
-unsigned int orTelemGetSize() {
+unsigned int vrTelemGetSize() {
     return sizeof (vrTelemStruct_t);
 }
