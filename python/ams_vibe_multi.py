0c0b52a097c65d2b6214a70a1b9c4ef7d3f11ec5
Andrew Pullin <pullin@berkeley.edu>
2015-05-18 18:38:59 -0700

    Shifting multi-running to its own file.

--- python/ams_vibe_multi.py
+++ python/ams_vibe_multi.py
@@ -8,6 +8,7 @@ Contents of this file are copyright Andrew Pullin, 2013
 from lib import command
 import time,sys,os
 import serial
+import numpy as np
 
 # Path to imageproc-settings repo must be added
 sys.path.append(os.path.dirname("../../imageproc-settings/"))
@@ -42,12 +43,12 @@ def main():
     #Verify all robots can be queried
     verifyAllQueried()  #exits on failure
     
-    motorgains = [1800,0,0,0,0, 1800,0,0,0,0]
+    motorgains = [1500,0,0,0,0, 1500,0,0,0,0]
     #motorgains = [0,0,0,0,0, 0,0,0,0,0]
     R1.setMotorGains(motorgains)
     
     # example , 0.1s lead in + 2s run + 0.1s lead out
-    EXPERIMENT_RUN_TIME_MS     = 4000 #ms
+    EXPERIMENT_RUN_TIME_MS     = 3000 #ms
     EXPERIMENT_LEADIN_TIME_MS  = 100  #ms
     EXPERIMENT_LEADOUT_TIME_MS = 100  #ms
     
@@ -61,36 +62,53 @@ def main():
     
     PHASE_ZERO = 0
     PHASE_180 = 1.0
-    freqL = 20
-    freqR = 20
+    freqL = 25
+    freqR = 25
     #amp = 2000
-    R1.setAMSvibe(channel=1, frequency=freqL, amplitude = 5000, offset = 000, phase = PHASE_ZERO)
-    R1.setAMSvibe(channel=2, frequency=freqR, amplitude = 5000, offset = 3000, phase = PHASE_ZERO)
+    #R1.setAMSvibe(channel=1, frequency=freqL, amplitude = 5000, offset = 0, phase = PHASE_ZERO)
+    #R1.setAMSvibe(channel=2, frequency=freqR, amplitude = 5000, offset = 0, phase = PHASE_ZERO)
     
-    # Pause and wait to start run, including lead-in time
+    
+    
+    # Sleep for a lead-in time before any motion commands
+    time.sleep(EXPERIMENT_LEADIN_TIME_MS / 1000.0)
+    
+    DEADTIME = 6.0
+    
+    freqs = [10, 15, 20, 25, 30, 35]
+    nrep = 5;
+    
+    flist = np.array([[i]*nrep for i in freqs]).flatten().tolist()
+    
+    nruns = len(flist)
+    est_time = nruns * (EXPERIMENT_LEADIN_TIME_MS + EXPERIMENT_LEADOUT_TIME_MS + EXPERIMENT_RUN_TIME_MS)/1000.0 + (nruns-1)*DEADTIME
+    print
+    
+    print "Total estimated time:", est_time,"seconds,",nruns,"trials."
+    
     print "\n  ***************************\n  *******    READY    *******\n  Press ENTER to start run ...\n  ***************************"
     raw_input("")
-    print ""
+    
+    for f in flist:
+        freqL = f
+        freqR = f
+        #amp = 2000
+        R1.setAMSvibe(channel=1, frequency=freqL, amplitude = 5000, offset = 0, phase = PHASE_ZERO)
+        R1.setAMSvibe(channel=2, frequency=freqR, amplitude = 5000, offset = 0, phase = PHASE_ZERO)
+        
+        # Sleep for a lead-in time before any motion commands
+        time.sleep(EXPERIMENT_LEADIN_TIME_MS / 1000.0)
+        R1.startTimedRun( EXPERIMENT_RUN_TIME_MS ) #Faked for now, since pullin doesn't have a working VR+AMS to test with
+        time.sleep(EXPERIMENT_RUN_TIME_MS / 1000.0)  #argument to time.sleep is in SECONDS
+        
+        #dead time between runs
+        time.sleep(DEADTIME)
 
     # Initiate telemetry recording; the robot will begin recording immediately when cmd is received.
     for r in shared.ROBOTS:
         if r.SAVE_DATA:
             r.startTelemetrySave()
     
-    # Sleep for a lead-in time before any motion commands
-    time.sleep(EXPERIMENT_LEADIN_TIME_MS / 1000.0)
-    
-    ######## Motion is initiated here! #######
-    
-    R1.startTimedRun( EXPERIMENT_RUN_TIME_MS ) #Faked for now, since pullin doesn't have a working VR+AMS to test with
-    time.sleep(EXPERIMENT_RUN_TIME_MS / 1000.0)  #argument to time.sleep is in SECONDS
-    
-    #R1.setAMSvibe(channel=1, frequency=freqL, amplitude = 0, offset = 0, phase = ZERO_PHASE)
-    #R1.setAMSvibe(channel=2, frequency=freqR, amplitude = 0, offset = 0, phase = ZERO_PHASE)
-    
-    ######## End of motion commands   ########
-    
-    # Sleep for a lead-out time after any motion
     time.sleep(EXPERIMENT_LEADOUT_TIME_MS / 1000.0) 
     
     for r in shared.ROBOTS:
